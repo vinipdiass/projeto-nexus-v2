8f888b0f6fe50be17b101ae9217c80ebf38687ed
Vinícius Dias <108445811+vinipdiass@users.noreply.github.com>
2024-10-07 20:23:56 -0300

    mongodb implementado

--- server.js
+++ server.js
@@ -1,126 +1,115 @@
 const express = require('express');
-const app = express();
-const fs = require('fs');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
 
 // Configurações
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// Caminho para o arquivo data.json
-const dataFilePath = './data.json';
+// Conectando ao MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Conectado ao MongoDB');
+}).catch((err) => {
+    console.error('Erro ao conectar ao MongoDB:', err);
+});
+
+// Definindo o modelo de construção
+const construcaoSchema = new mongoose.Schema({
+    id: Number,
+    nome: String,
+    latitude: Number,
+    longitude: Number,
+    estoque: [
+        {
+            item: String,
+            quantidade: Number,
+        },
+    ],
+});
+
+const Construcao = mongoose.model('Construcao', construcaoSchema);
 
 // Endpoint para obter todas as construções
-app.get('/construcoes', (req, res) => {
-    fs.readFile(dataFilePath, 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Erro ao ler o arquivo de dados.');
-        } else {
-            res.send(JSON.parse(data));
-        }
-    });
+app.get('/construcoes', async (req, res) => {
+    try {
+        const construcoes = await Construcao.find();
+        res.send(construcoes);
+    } catch (error) {
+        res.status(500).send('Erro ao obter as construções.');
+    }
 });
 
 // Endpoint para obter uma construção por ID
-app.get('/construcoes/:id', (req, res) => {
-    const construcaoId = req.params.id; // Manter como string
-    fs.readFile(dataFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Erro ao ler o arquivo de dados:', err);
-            res.status(500).send('Erro ao ler o arquivo de dados.');
+app.get('/construcoes/:id', async (req, res) => {
+    const construcaoId = req.params.id;
+    try {
+        const construcao = await Construcao.findOne({ id: construcaoId });
+        if (construcao) {
+            res.send(construcao);
         } else {
-            const construcoes = JSON.parse(data);
-            const construcao = construcoes.find(c => c.id.toString() === construcaoId);
-            if (construcao) {
-                res.send(construcao);
-            } else {
-                res.status(404).send('Construção não encontrada.');
-            }
+            res.status(404).send('Construção não encontrada.');
         }
-    });
+    } catch (error) {
+        res.status(500).send('Erro ao obter a construção.');
+    }
 });
 
-
-
 // Endpoint para adicionar uma nova construção
-app.post('/construcoes', (req, res) => {
+app.post('/construcoes', async (req, res) => {
     const novaConstrucao = req.body;
-    fs.readFile(dataFilePath, 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('Erro ao ler o arquivo de dados.');
-        } else {
-            const construcoes = JSON.parse(data);
-            construcoes.push(novaConstrucao);
-            fs.writeFile(dataFilePath, JSON.stringify(construcoes, null, 2), err => {
-                if (err) {
-                    res.status(500).send('Erro ao salvar os dados.');
-                } else {
-                    res.send(novaConstrucao);
-                }
-            });
-        }
-    });
+    try {
+        const construcao = new Construcao(novaConstrucao);
+        await construcao.save();
+        res.send(construcao);
+    } catch (error) {
+        res.status(500).send('Erro ao salvar a construção.');
+    }
 });
 
 // Endpoint para atualizar o estoque de uma construção
-app.put('/construcoes/:id/estoque', (req, res) => {
-    const construcaoId = req.params.id; // Manter como string
+app.put('/construcoes/:id/estoque', async (req, res) => {
+    const construcaoId = req.params.id;
     const novoEstoque = req.body;
-    fs.readFile(dataFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Erro ao ler o arquivo de dados:', err);
-            res.status(500).send('Erro ao ler o arquivo de dados.');
+    try {
+        const construcao = await Construcao.findOneAndUpdate(
+            { id: construcaoId },
+            { $set: { estoque: novoEstoque } },
+            { new: true }
+        );
+        if (construcao) {
+            res.send(construcao);
         } else {
-            let construcoes = JSON.parse(data);
-            let construcao = construcoes.find(c => c.id.toString() === construcaoId);
-            if (construcao) {
-                construcao.estoque = novoEstoque;
-                fs.writeFile(dataFilePath, JSON.stringify(construcoes, null, 2), err => {
-                    if (err) {
-                        console.error('Erro ao salvar os dados:', err);
-                        res.status(500).send('Erro ao salvar os dados.');
-                    } else {
-                        res.send(construcao);
-                    }
-                });
-            } else {
-                res.status(404).send('Construção não encontrada.');
-            }
+            res.status(404).send('Construção não encontrada.');
         }
-    });
+    } catch (error) {
+        res.status(500).send('Erro ao atualizar o estoque.');
+    }
 });
-
-
 
 // Endpoint para remover uma construção pelo ID
-app.delete('/construcoes/:id', (req, res) => {
-    const construcaoId = req.params.id; // Manter como string
-    fs.readFile(dataFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Erro ao ler o arquivo de dados:', err);
-            res.status(500).send('Erro ao ler o arquivo de dados.');
+app.delete('/construcoes/:id', async (req, res) => {
+    const construcaoId = req.params.id;
+    try {
+        const construcao = await Construcao.findOneAndDelete({ id: construcaoId });
+        if (construcao) {
+            res.send({ message: 'Construção removida com sucesso.' });
         } else {
-            let construcoes = JSON.parse(data);
-            const index = construcoes.findIndex(c => c.id.toString() === construcaoId);
-            if (index !== -1) {
-                construcoes.splice(index, 1);
-                fs.writeFile(dataFilePath, JSON.stringify(construcoes, null, 2), err => {
-                    if (err) {
-                        console.error('Erro ao salvar os dados:', err);
-                        res.status(500).send('Erro ao salvar os dados.');
-                    } else {
-                        res.send({ message: 'Construção removida com sucesso.' });
-                    }
-                });
-            } else {
-                res.status(404).send('Construção não encontrada.');
-            }
+            res.status(404).send('Construção não encontrada.');
         }
-    });
+    } catch (error) {
+        res.status(500).send('Erro ao remover a construção.');
+    }
 });
-
 
 // Iniciando o servidor
 app.listen(3000, () => {
